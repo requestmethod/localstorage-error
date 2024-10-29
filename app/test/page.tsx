1a57614c74d8963ad28893ebf4bfdc08db0552b7
requestmethod <requestmethod@proton.me>
2024-10-29 16:47:02 -0500

    add test link

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
-import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      Test Page
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
@@ -23,10 +23,6 @@ export default function Home() {
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
-
-        <Link href="/test">
-          Clicking this link should create an Application Error
-        </Link>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
